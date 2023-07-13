ddca50519653836c3208068821dd365479d0eb16
Marat Kotik <mkotik97@gmail.com>
2023-07-12 21:54:31 -0400

    reduce height of features panel

--- src/app/FeaturesPanel/FeaturesPanel.tsx
+++ src/app/FeaturesPanel/FeaturesPanel.tsx
@@ -5,7 +5,7 @@ import analyze from "./analyze.svg";
 
 const FeaturesPanel = () => {
   return (
-    <div className="min-h-screen bg-[#E7E6E3] pb-[70px]">
+    <div className="min-h-[500px] bg-[#E7E6E3] pb-[70px]">
       <div className="flex flex-wrap justify-center mt-20 gap-7">
         <Panel text="Track Key Metrics" logo={metrics} />
         <Panel text="Reveal Unfollowers" logo={unfollower} />
